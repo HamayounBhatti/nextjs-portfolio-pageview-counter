862b8525e02466c7e7db39accfe417f9d5bb6021
HamayounBhatti <88651217+HamayounBhatti@users.noreply.github.com>
2025-01-14 18:59:37 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,10 +30,10 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-9xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display  whitespace-nowrap bg-clip-text ">
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Hamayoun Bhatti
       </h1>
-      <h6 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display whitespace-nowrap bg-clip-text ">
+      <h6 className="py-3.5 px-0.5 z-10 text-2xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-2xl whitespace-nowrap bg-clip-text ">
         Software Engineer
       </h6>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
