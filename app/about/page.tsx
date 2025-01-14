30c0ce43cbed414005e10fb7ac14b4eb3a7bb973
HamayounBhatti <88651217+HamayounBhatti@users.noreply.github.com>
2025-01-14 12:09:16 +0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,38 +4,17 @@ import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
-// const socials = [
-//     {
-//         icon: <Twitter size={20} />,
-//         href: "https://twitter.com/chronark_",
-//         label: "Twitter",
-//         handle: "@chronark_",
-//     },
-//     {
-//         icon: <Mail size={20} />,
-//         href: "mailto:dev@chronark.com",
-//         label: "Email",
-//         handle: "dev@chronark.com",
-//     },
-//     {
-//         icon: <Github size={20} />,
-//         href: "https://github.com/chronark",
-//         label: "Github",
-//         handle: "chronark",
-//     },
-// ];
-
 export default function Example() {
     return (
         <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
             <Navigation />
             <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
                 <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
-                    {socials.map((s) => (
+                  
                         <Card>
                             <p>I'm Hamayoun Bhatti, a passionate Web Developer. With 2 Years Of experience in this field, I'm driven by a relentless pursuit of excellence. I thrive on challenges and am inspired by the potential to solve problems, innovate, and create meaningful solutions, and my work reflects my dedication to contribute to sustainable design solutions that benefit society. Explore my portfolio to see how I bring creativity and expertise to every project. Let's connect and collaborate to make great things happen!</p>
                         </Card>
-                    ))}
+                 
                 </div>
             </div>
         </div>
